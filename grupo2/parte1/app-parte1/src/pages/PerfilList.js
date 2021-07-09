100aaf4df6d21b117bd5d4e678df80e6edcd5768
Alexandre <naitwa.alexandre@gmail.com>
2021-07-09 20:04:49 -0300

    done

--- grupo2/parte1/app-parte1/src/pages/PerfilList.js
+++ grupo2/parte1/app-parte1/src/pages/PerfilList.js
@@ -8,9 +8,10 @@ import { getPERFIS, toggleLoad } from '../redux/actions';
 
 function PerfilList() {
   const dispatch = useDispatch();
-  const state = useSelector(state => state.PerfisReducer)
+  const state = useSelector(state => state.PerfisReducer);
   const { perfis, loading, name } = state;
   const [limit, setLimit] = useState(5);
+  const [filtredPerfis, setFiltredPerfis] = useState([]);
   const url = 'https://api.github.com/users';
 
   const fetchPerfil = async () => {
@@ -28,12 +29,16 @@ function PerfilList() {
     fetchPerfil();
   }, []);
 
+  useEffect(() => {
+    setFiltredPerfis(perfis.filter((perfil) => perfil.login.toLowerCase().includes(name.toLowerCase())));
+  }, [perfis, name])
+
   if (loading) {
     return <Loading />
   }
   return (
     <WrapperPerfis>
-      { perfis.slice(0, limit ? limit : perfis.length).map(perfil => (
+      { filtredPerfis.slice(0, limit ? limit : perfis.length).map(perfil => (
         <Perfil key={ perfil.id } { ...perfil } />
       ))}
     </WrapperPerfis>
